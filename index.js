ac23045eece96fed0ce84f1689e04a532a0d976b
tyler knapp <tgknapp11@gmail.com>
2019-05-14 15:59:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// write your code below!
+function happyHolidays() {
+  return("Happy holidays!")
+}
+
+function happyHolidaysTo (string) {
+  var name = "Anne"
+  return(`Happy Holidays, ${name}!`)
+}
+
+function happyHolidayTo (string) {
+  var holiday = "Birthday"
+  var name = "Anne"
+  return(`Happy ${holiday}, ${name}`)
+}
+
+function holidayCountdown (string, Int) {
+  var days = 60;
+  var holiday = "your birthday"
+  return(`It's ${days} until ${holiday}`)
+}
